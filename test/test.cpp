94790346528b2543d03b33d1a698536a6672a11c
zzyxuan <190507538@qq.com>
2021-11-14 15:57:05 +0800

    add fgh

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int a,b,c,d,e;
-	cin>>a>>b>>c>>d>>e;
-	cout<<a+b+c+d+e<<endl;
+	int a,b,c,d,e,f,g,h;
+	cin>>a>>b>>c>>d>>e>>f>>g>>h;
+	cout<<a+b+c+d+e+f+g+h<<endl;
 }
